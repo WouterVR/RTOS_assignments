914dc596b6860c1a7ab0f190728a7d3acaf52595
WouterVR <vanrompaey.wouter@gmail.com>
2021-03-05 18:33:55 +0100

    Ass1_3 completed

--- assignment1/Ass1_3.c
+++ assignment1/Ass1_3.c
@@ -4,18 +4,26 @@
 
 int findPrime(int topNumber){
     int i;
+    int amountOfPrimes = 0;
     pid_t pid = getpid();
-    for(i=1; i<=topNumber; ++i){ //looping throug all the numbers untill the highes number
-        int j,m=0,flag=0;
-        m=i/2;    
-        for(j=2;j<=m;j++){ //Checking if prime or not
-            if(i%j==0){     
-                flag=1;
+    for(i=2; i<=topNumber; i++){ //looping throug all the numbers untill the highes number
+        //printf("Checking the number: %d .\n", i);
+        int j;
+        int notPrime=0;
+        for(j=2;j<=i/2;j++){ //Checking if prime or not
+            //printf("Going to check %d percentage with %d . \n",i,j);
+            if(i%j==0){
+                notPrime=1;
+                break;
             }
         }
-        if(flag==0) printf("child (PID= %d ) announces that %d is prime. \n",pid, i);     
-        return 0;  
+        if(notPrime==0) {
+            printf("child (PID= %d ) announces that %d is prime. \n",pid, i);
+            amountOfPrimes++;
+        }
     }
+    printf("*** child process %d is ready and found that there are %d prime numbers under %d. *** \n",pid, amountOfPrimes, topNumber);
+    return 0;
 }
 
 int main() {
